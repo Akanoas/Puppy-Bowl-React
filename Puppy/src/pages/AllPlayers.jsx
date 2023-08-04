4152335ff25128b696bb90c11b72d541775eddc0
Abdoul Akano <akano1nassirou@gmail.com>
2023-08-04 16:31:24 -0400

    data fetching

--- Puppy/src/pages/AllPlayers.jsx
+++ Puppy/src/pages/AllPlayers.jsx
@@ -6,8 +6,13 @@ export default function AllPlayers(){
     
     
     useEffect(() =>{
-        
+        async function fetchData(){
+            const data = await fetchAllPlayers()
+            setPlayers(data)
 
+        }
+
+        fetchData()
     }, [])
     
     
@@ -17,6 +22,18 @@ export default function AllPlayers(){
     return(
         <div>
             <h1>AllPlayers</h1>
+
+            {
+                players.map(player => {
+                    <div key={ player.key}>
+                        <h2>name:{player.name}</h2>
+                        <p>breed:{player.breed} </p>
+                        <img src = {player.image} alt={players[0].name} />
+                    </div>
+                })
+
+
+            }
 
         </div>
 
